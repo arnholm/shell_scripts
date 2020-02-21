17fa6ad6f35e8f01e9de57c2e0e5d069ffbd56ac
Carsten Arnholm <arnholm@arnholm.org>
2020-02-21 09:04:09 +0100

    Make scripts more general

--- cross-pi/cross-pi-codeblocks.sh
+++ cross-pi/cross-pi-codeblocks.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 # cross-pi-codeblocks.sh
+# This script downloads, builds and installs Code::Blocks under debian/ubuntu
+# Run this script with sudo privilege
 
-cd ~
-mkdir build_codeblocks
-cd build_codeblocks/
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+#build in a temporary, disposable folder
+mkdir /tmp/build_codeblocks
+pushd /tmp/build_codeblocks/ > /dev/null 
+#
+# fetch latest from github
 git clone https://github.com/obfuscated/codeblocks_sf
-cd codeblocks_sf/
+#
+# build the *.deb files
+cd ./codeblocks_sf/
 dpkg-buildpackage -us -uc
+#
+# install from the just created *.deb files
 cd ..
-sudo dpkg -i *.deb
-cd ~
+dpkg -i *.deb
+#
+# return to original directory
+popd > /dev/null
 echo "Code::Blocks has been installed! "
--- cross-pi/cross-pi-toolchain.sh
+++ cross-pi/cross-pi-toolchain.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # cross-pi-toolchain.sh
 # Run this script with sudo privilege
+# This script downloads & installs Ubuntu/Raspberry PI cross compiler toolchain
+# NOTE: The toolchain has to be in /opt/cross-pi-gcc since it's not location independent.
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-# Fetch the latest raspberry PI toolchain from github
+#
+# Fetch the latest raspberry PI toolchain from GitHub
 wget https://github.com/Pro/raspi-toolchain/releases/latest/download/raspi-toolchain.tar.gz -P /tmp/raspi-toolchain
-# Extract the toolchain
-# Note: The toolchain has to be in /opt/cross-pi-gcc since it's not location independent.
-sudo tar xfz /tmp/raspi-toolchain/raspi-toolchain.tar.gz --strip-components=1 -C /opt
+#
+# Install the toolchain 
+tar xfz /tmp/raspi-toolchain/raspi-toolchain.tar.gz --strip-components=1 -C /opt
+#
 echo "Raspberry PI toolchain has been installed! "
 ls -l /opt/cross-pi-gcc
